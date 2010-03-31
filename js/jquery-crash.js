c2aa465f85e5ab2b0e0cd7d78902103dbd038c36
Shawn Price <shawnp@gmail.com>
2010-03-31 14:31:50 -0700

    add ie6 crash support to misery

---
+++ js/jquery-crash.js
@@ -0,0 +1,15 @@
+/**
+ * jQuery Crash (http://mktgdept.com/jquery-crash)
+ * A jQuery plugin to crash IE6.
+ *
+ * v0.0.2 - 5 March 2010
+ *
+ * Copyright (c) 2009 Chad Smith (http://twitter.com/chadsmith)
+ * Dual licensed under the MIT and GPL licenses.
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * Use $.crash();
+ *
+ **/
+;jQuery.crash=function(x){for(x in document.open);};
